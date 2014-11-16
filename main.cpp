8d6281a58462a8428c39fcb039b64a64c0e3fc6b
Derek Ng <18043196@qq.com>
2014-11-16 23:55:44 +0800

    version 1.2a

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include <stdio.h>
+#include <math.h>
 
 using namespace std;
 
-int main()
+float AreaOfTriangle( float a, float b, float c )
 {
-    cout << "Hello Big World!" << endl;
+    float p = ( a + b + c ) / 2.0f;
+    return sqrt( p * ( p - a ) * ( p - b ) * ( p - c ) );
+}
+
+int main(void)
+{
+    printf( "Hello git!\n" );
+    printf( "The area of a triangle is: %f\n",
+            AreaOfTriangle( 6.0f, 6.0f, 6.0f ) );
     return 0;
 }
+
+//int main()
+//{
+//    cout << "Hello Big World!" << endl;
+//    return 0;
+//}
 
